da633e655536486f1e41337398e9e730069479e5
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-20 14:51:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+/* In pieno tema natalizio sfruttiamo le timing functions per  fare il conto alla rovescia per il Natale!  
+Ogni secondo il nostro countdown dovrà scalare fino alla mezzanotte del 25 dicembre! */
+
+
