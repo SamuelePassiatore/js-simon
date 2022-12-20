5f3c98c4bef02f61508f584027e6834c3e5cccde
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-20 16:21:20 +0100

    take DOM's elements

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /* In pieno tema natalizio sfruttiamo le timing functions per  fare il conto alla rovescia per il Natale!  
 Ogni secondo il nostro countdown dovrà scalare fino alla mezzanotte del 25 dicembre! */
 
-
+// Prendo gli elementi dal DOM
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
