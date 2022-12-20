bc4f1a12c27df1cc9c476c05dd86b2a809e00b51
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-20 19:14:30 +0100

    timer function & set interval

--- script.js
+++ script.js
@@ -15,14 +15,6 @@ const christmasDate = new Date('December 25 2022');
 const christmasDateInMs = christmasDate.getTime();
 ////console.log(christmasDateInMs);
 
-// Creo la variabile del giorno attuale in millisecondi
-const currentDayInMs = new Date().getTime();
-////console.log(currentDayInMs);
-
-// Differenza tra giorno di natale e giorno attuale
-const difference = christmasDateInMs - currentDayInMs;
-console.log(difference);
-
 // Trasformo tutte le variabili del tempo in millisecondi
 const secondsInMs = 1000;
 const minutesInMs = 60 * secondsInMs;
@@ -30,26 +22,41 @@ const hoursInMs = 60 * minutesInMs;
 const daysInMs = 24 * hoursInMs;
 ////console.log(secondsInMs, minutesInMs, hoursInMs, daysInMs);
 
-// Calcoli con l'arrotondamento
-// Giorni mancanti
-const missingDays = Math.floor( difference / daysInMs);
-console.log(missingDays);
-// Ore mancanti
-const missingHours = Math.floor((difference % daysInMs) / hoursInMs);
-console.log(missingHours);
-// Minuti mancanti
-const missingMinutes = Math.floor((difference % hoursInMs) / minutesInMs);
-console.log(missingMinutes);
-// Secondi mancanti
-const missingSeconds = Math.floor((difference % minutesInMs) / secondsInMs);
-console.log(missingSeconds);
+// Funzioni
+function timer() {
+   
+    // Creo la variabile del giorno attuale in millisecondi
+    const currentDayInMs = new Date().getTime();
+    ////console.log(currentDayInMs);
 
-// Stampo in pagina tutti i dati ottenuti
-// Stampo in pagina i giorni
-daysElement.innerText = missingDays;
-// Stampo in pagina le ore
-hoursElement.innerText = missingHours;
-// Stampo in pagina i giorni
-minutesElement.innerText = missingMinutes;
-// Stampo in pagina i giorni
-secondsElement.innerText = missingSeconds;
+    // Differenza tra giorno di natale e giorno attuale
+    const difference = christmasDateInMs - currentDayInMs;
+
+    // Calcoli con l'arrotondamento
+    // Giorni mancanti
+    const missingDays = Math.floor( difference / daysInMs);
+    ////console.log(missingDays);
+    // Ore mancanti
+    const missingHours = Math.floor((difference % daysInMs) / hoursInMs);
+    ////console.log(missingHours);
+    // Minuti mancanti
+    const missingMinutes = Math.floor((difference % hoursInMs) / minutesInMs);
+    ////console.log(missingMinutes);
+    // Secondi mancanti
+    const missingSeconds = Math.floor((difference % minutesInMs) / secondsInMs);
+    ////console.log(missingSeconds);
+
+    // Stampo in pagina tutti i dati ottenuti
+    // Stampo in pagina i giorni
+    daysElement.innerText = missingDays;
+    // Stampo in pagina le ore
+    hoursElement.innerText = missingHours;
+    // Stampo in pagina i giorni
+    minutesElement.innerText = missingMinutes;
+    // Stampo in pagina i giorni
+    secondsElement.innerText = missingSeconds;   
+}
+
+// Variabile con il conteggio automatico
+const counterTimer = setInterval(timer, 1000);
+
